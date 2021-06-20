f01176c8ade7bbeb3505ef5bcdad7134a451794a
fsulta16 <fsulta16@my.centennialcollege.ca>
2021-06-20 18:13:13 -0400

    added db config for mongoDB Atlas

---
+++ .history/Server/Config/db_20210620175708.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost/contact_list"
+export const RemoteURI = "mongodb+srv://admin: hwv1ZTAvBUU1O8Sf@cluster-comp229.sctvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const HostName = "RemoteHost";
+export const Secret = "someSecret";
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/contact_list";
-exports.RemoteURI = "mongodb+srv://admin:cd4xEhO1o8mnp6JO@cluster-comp229.1jttx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://admin:hwv1ZTAvBUU1O8Sf@cluster-comp229.sctvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
